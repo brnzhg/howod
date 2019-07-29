7a980a0f57fe6f94723e5b495b5972edca76b688
Brian Zhang <brnzhg@gmail.com>
2019-07-28 23:45:24 -0500

    added some small helpers

--- src/Neldermead.hs
+++ src/Neldermead.hs
@@ -14,11 +14,14 @@ module Neldermead (
   , haftkaGurdalSimplex
   ) where
 
+import Data.Ord (comparing)
 import Data.Function (on)
 import Data.Bool (bool)
 import Data.Maybe (fromMaybe)
 import Data.Monoid (Sum)
 import qualified Data.Foldable as FLD
+
+import Control.Arrow ((&&&))
 
 import qualified Numeric.Interval.NonEmpty as IVL
 
@@ -28,6 +31,7 @@ import Data.Singletons.TypeLits
 
 import qualified Data.Vector.Sized as V
 import qualified Data.Vector.Storable.Sized as SV
+--import qualified Data.Vector.Mutable.Sized as MV
 import Numeric.LinearAlgebra.Static
 import Numeric.LinearAlgebra.Static.Vector
 import GHC.TypeNats
@@ -50,34 +54,60 @@ newtype NMSimplex n = NMSimplex { fOrderedVertices :: V.Vector n (NMVertex n) }
 newtype Simplex n = Simplex { xA :: L n (n + 1) }
     deriving (Show)
 
-newtype BoxConstraints n = BoxConstraints { 
-  unBoxConstraints :: V.Vector n (IVL.Interval ℝ)
-  }
---TODO ok for these to be impossible? guess so
---data BoxConstraints n = BoxConstraints
---  { lb :: !(V.Vector n (Maybe ℝ))
---  , ub :: !(V.Vector n (Maybe ℝ))
---  }
+newtype BoxConstraints n = BoxConstraints { unBoxConstraints :: V.Vector n (IVL.Interval ℝ) }
+
+data NMScalars = NMScalars {
+  reflectWeight :: ℝ
+  , expandWeight :: ℝ
+  , contractWeight :: ℝ
+  , shrinkWeight :: ℝ
+}
+
+--TODO convergeTest should be monadic to allow writing details
+--
+data NMEnv n = MEnv {
+  scalars :: NMScalars
+  , simplexConvergeTest :: NMSimplex n -> Bool
+}
 
 
-rVecIso :: forall n. KnownNat n => LI.Iso' (R n) (SV.Vector n ℝ)
+--nextNMSimplex :: forall n. KnownNat n => NMSimplex n -> 
+
+
+
+--TODO may need MonadReader and MonadState, and allow objective to be in any monad
+rVecIso :: KnownNat n => LI.Iso' (R n) (SV.Vector n ℝ)
 rVecIso = LI.iso rVec vecR
 
 projectToBox :: KnownNat n => BoxConstraints n -> R n -> R n
-projectToBox (BoxConstraints bc) = L.over rVecIso $ SV.imap $ IVL.clamp . V.index bc
+projectToBox (BoxConstraints bc) = 
+  L.over rVecIso $ SV.imap $ IVL.clamp . V.index bc
 
---TODO make initial in bounds
---for each col vector (step vector, x0 is zero so sbutract x0)
---  is legal against UP, is legal against LB
---      do this by finding max negative diff
---  if both negative, take ratio of bound and current, divide whole vector
 
-rightAngledSimplex :: forall n. KnownNat n => BoxConstraints n -> ℝ -> R n -> Simplex n
-rightAngledSimplex size x0 = undefined
+--awkward because cant deconstruct interval into inf sup
+--and cant zip storable and not storable
+rightAngledSimplexInBox :: forall n. KnownNat n 
+  => BoxConstraints n -> R n -> R n -> Simplex n
+rightAngledSimplexInBox (BoxConstraints bc) step x0 = Simplex
+  $ ((x0 `outer` 1) + diag step') ||| col x0
+  where
+    (step' :: R n) = vecR
+      $ SV.generate
+      $ \i -> let stepBounds = V.index bc i - IVL.singleton (SV.index vx0 i)
+                  si = abs $ SV.index vstep i
+              in clampStep ((IVL.inf &&& IVL.sup) stepBounds) si
+    clampStep (stepLb, stepUb) stepi 
+      | stepi <= stepUb = stepi
+      | -stepi >= stepLb = -stepi
+      | -stepLb < stepUb = max 0 $ 0.5 * stepUb
+      | otherwise = min 0 $ 0.5 * stepLb
+    vstep = rVec step
+    vx0 = rVec x0
+    
 
 haftkaGurdalSimplex :: forall n. KnownNat n => ℝ -> R n -> Simplex n
 haftkaGurdalSimplex size x0 = Simplex . colsL 
-  $ V.generate $ \j -> x0 L.& rVecIso L.%~ (SV.imap (\i x -> x + step i j))
+  $ V.generate $ \j -> x0 L.& rVecIso L.%~ (SV.imap $ \i x -> x + step i j)
   where
     step i j
       | toInteger j == toInteger n' = 0
@@ -100,12 +130,6 @@ centroidL = (#> (1.0 / l))
   where
     l = fromIntegral . fromSing $ (sing :: Sing m)
 
---TODO write version using mean
---toRows, map mean, list to vec
-centroidL2 :: forall m n. Sing m -> Sing n -> L m n -> R m
-centroidL2 (sm@SNat) (sn@SNat) = (#> (1.0 / l))
-  where
-    l = fromIntegral . fromSing $ (sing :: Sing m)
 
 realWithinRelTol :: ℝ -> ℝ -> ℝ -> Bool
 realWithinRelTol relTol x y = (<= 0.5 * (x `absAdd` y)) . abs $ x - y
@@ -119,11 +143,20 @@ realWithinTols relTol absTol x y = relTolCheck && absTolCheck
     absTolCheck = fromMaybe True $ (<= abs (x - y)) <$> absTol
 
 
---center + max step to given vertex for each dimension
-simplexRadiusTo :: KnownNat n => Simplex n -> R n -> R n
-simplexRadiusTo (Simplex xs) c = vecR $ SV.generate (V.index v)
+simplexRadiusTo :: (KnownNat n, Foldable t) => t (R n) -> R n -> R n
+simplexRadiusTo xs c = vecR 
+  $ SV.generate 
+  $ \i -> FLD.foldl' (\acc -> (max `on` abs) acc . distci i . rVec) 0 xs
   where 
-    v = (SV.maximum . rVec) <$> lRows xsDiff -- Vector, need to convert to (R n)
+    distci i v = SV.index v i - SV.index vc i
+    vc = rVec c
+
+
+--center + max step to given vertex for each dimension
+simplexRadiusToL :: KnownNat n => Simplex n -> R n -> R n
+simplexRadiusToL (Simplex xs) c = vecR $ SV.generate (V.index v)
+  where 
+    v = (SV.maximumBy (comparing abs) . rVec) <$> lRows xsDiff -- Vector, need to convert to (R n)
     xsDiff = xs - (c `outer` 1) -- vector not promoted to matrix
 
 affineCombWith :: KnownNat n => R n -> ℝ -> R n -> R n
@@ -132,6 +165,12 @@ affineCombWith x cy y = x + rnScale cy (y - x)
 --put in helper class, maybe use coerce or something
 rnScale :: KnownNat n => ℝ -> R n -> R n
 rnScale c = L.over rVecIso $ SV.map (c *)
---affinecombwithclamp
---anchor, x, combweight, bounds
---anchor + weight * (x - anchor)
+
+
+--TODO write version using mean
+--toRows, map mean, list to vec
+centroidL2 :: forall m n. Sing m -> Sing n -> L m n -> R m
+centroidL2 (sm@SNat) (sn@SNat) = (#> (1.0 / l))
+  where
+    l = fromIntegral . fromSing $ (sing :: Sing m)
+
